1379d845b0721184f2cead516ae39961574d1cb7
Md. Emon Mahmud <emonmahmud083@gmail.com>
2026-03-12 23:40:18 +0600

    Homepage Hero, Categories, Featured & Latest jobs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,5 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
   );
 }
--- src/types/css.d.ts
+++ src/types/css.d.ts
@@ -3,3 +3,18 @@ declare module "*.css" {
   export default content;
 }
 
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
